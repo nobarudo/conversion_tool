5bfaa6131d29ab531aab645142337cbd3e0ee894
unknown <esukab@gmail.com>
2019-05-12 03:40:40 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"net/url"
+
+	"github.com/lxn/walk"
+	. "github.com/lxn/walk/declarative"
+)
+
+func main() {
+	var inTE, outTE *walk.TextEdit
+
+	MainWindow{
+		Title:   "convert_tool",
+		MinSize: Size{600, 400},
+		Layout:  VBox{},
+		Children: []Widget{
+			HSplitter{
+				Children: []Widget{
+					TextEdit{AssignTo: &inTE},
+					TextEdit{AssignTo: &outTE, ReadOnly: true},
+				},
+			},
+			PushButton{
+				Text: "変換",
+				OnClicked: func() {
+					outTE.SetText(url.QueryEscape(inTE.Text()))
+				},
+			},
+		},
+	}.Run()
+}
