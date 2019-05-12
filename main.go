e64c3f6bd4f7f8de19f04c43d9fb0fc37b554b4e
unknown <esukab@gmail.com>
2019-05-13 04:34:25 +0900

    add Base64

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/base64"
 	"log"
 	"net/url"
 
@@ -10,12 +11,23 @@ import (
 
 func main() {
 	var inTE, outTE *walk.TextEdit
+	var menu *walk.ComboBox
+
+	var menuItem = []string{
+		"URLエンコード",
+		"Base64",
+	}
 
 	MainWindow{
 		Title:   "convert_tool",
 		MinSize: Size{600, 400},
 		Layout:  VBox{},
 		Children: []Widget{
+			ComboBox{
+				AssignTo:     &menu,
+				Model:        menuItem,
+				CurrentIndex: 0,
+			},
 			HSplitter{
 				Children: []Widget{
 					TextEdit{AssignTo: &inTE},
@@ -27,17 +39,31 @@ func main() {
 					PushButton{
 						Text: "エンコード",
 						OnClicked: func() {
-							outTE.SetText(url.QueryEscape(inTE.Text()))
+							switch menu.CurrentIndex() {
+							case 0:
+								outTE.SetText(url.QueryEscape(inTE.Text()))
+							case 1:
+								outTE.SetText(base64.StdEncoding.EncodeToString([]byte(inTE.Text())))
+							}
 						},
 					},
 					PushButton{
 						Text: "デコード",
 						OnClicked: func() {
-							str, err := url.QueryUnescape(inTE.Text())
-							if err != nil {
-								log.Println(err)
+							switch menu.CurrentIndex() {
+							case 0:
+								str, err := url.QueryUnescape(inTE.Text())
+								if err != nil {
+									log.Println(err)
+								}
+								outTE.SetText(str)
+							case 1:
+								str, err := base64.StdEncoding.DecodeString(inTE.Text())
+								if err != nil {
+									log.Println("error: ", err)
+								}
+								outTE.SetText(string(str))
 							}
-							outTE.SetText(str)
 						},
 					},
 					PushButton{
